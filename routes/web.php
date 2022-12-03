404087d7bf75e3b926e38c8acfd72cedaf1a6f79
Meheady <hmmehedi55@gmail.com>
2022-12-04 03:25:42 +0600

    admin theme setup and logout

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,35 @@ use App\Http\Controllers\VendorController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Clear route cache
+Route::get('/route-cache', function() {
+    \Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache
+Route::get('/config-cache', function() {
+    \Artisan::call('config:cache');
+    return 'Config cache cleared';
+});
+
+// Clear application cache
+Route::get('/clear-cache', function() {
+    \Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache
+Route::get('/view-clear', function() {
+    \Artisan::call('view:clear');
+    return 'View cache cleared';
+});
+
+// Clear cache using reoptimized class
+Route::get('/optimize-clear', function() {
+    \Artisan::call('optimize:clear');
+    return 'View cache cleared';
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,14 +53,17 @@ Route::get('/login/facebook',[SocialController::class,'facebookRedirect'])->name
 Route::get('/login/facebook/callback',[SocialController::class,'loginWithFacebook']);
 
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified','role:user'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth','verified','role:user'])->name('dashboard');
+
 
 require __DIR__.'/auth.php';
 
+
 Route::middleware(['auth','role:admin'])->group(function (){
     Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout',[AdminController::class,'AdminLogout'])->name('admin.logout');
 
 });
 Route::middleware(['auth','role:vendor'])->group(function (){
